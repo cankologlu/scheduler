46f659fc4e6c0f76baf037300535fe39d409bd26
Can Kologlu <mckologlu@gmail.com>
2023-02-21 16:51:25 -0500

    add classnames to button props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
+// export default function Button(props) {
+//    const {children} = props;
+//    return (
+//    <button>{children}</button>
+//    );
+// }
+
 export default function Button(props) {
-   return <></>;
-}
+ 
+   const {confirm, danger} = props;
+   const buttonClass =  classNames("button", {"button--confirm":confirm, "button--danger":danger});
+ 
+   return <button onClick={props.onClick} disabled={props.disabled} className={buttonClass}>
+      {props.children}
+      </button>;
+ }
+
+//  onClick="alert('you kliked?');
+
