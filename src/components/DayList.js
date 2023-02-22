f89f827bbb6395644f5bf72b10498ebbcfa1267d
Can Kologlu <mckologlu@gmail.com>
2023-02-22 00:20:28 -0500

    lifted setday to parent

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,12 @@ import DayListItem from "components/DayListItem";
 export default function DayList (props) {
   const {days} = props;
 
+  const handleClick = (data) => { 
+    props.setDay(data)
+  }
+
   const parsedDays = days.map((day) => (
-    <DayListItem  key={day.id} {...day}/>
+    <DayListItem setDay={handleClick} key={day.id} {...day}/>
   ));
   return (
     <ul>{parsedDays}</ul>
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,12 +5,13 @@ import classNames from "classnames";
 export default function DayListItem(props) 
 
 { 
-  const {selected, spots} = props;
+  // const {selected, spots} = props;
   
-  const dayClass = classNames("day-list__item",{'--selected': selected , "--full": !spots})
+  const dayClass = classNames("day-list__item",{' --selected': props.selected , " --full": !props.spots});
+  
   return (
-    <li className={dayClass}>
-      <h2 onClick={() => props.setDay(props.name)} className="text-regular">{props.name}</h2>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text-regular">{props.name}</h2>
       {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
       {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
       {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
