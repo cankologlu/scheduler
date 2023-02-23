fdd9e949de28d1421fb533aa6e449df4a161a94c
Can Kologlu <mckologlu@gmail.com>
2023-02-23 14:12:09 -0500

    add the show and empty to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,10 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
-export default function Appointment() {
-  return <article className="appointment"></article>;
+export default function Appointment(props) {
+  const {time, interview} = props;
+  return <article className="appointment">
+    <Header time={time}/>
+    {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty/>}
+  </article>;
 }
